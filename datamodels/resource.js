99c6860e2e1b3a13276f3763bc37eb3cde80e08f
Lucas Magalhães <lucoder618@gmail.com>
2023-08-10 01:24:05 -0300

    feat: resource description field incompleted

--- datamodels/resource.js
+++ datamodels/resource.js
@@ -2,7 +2,8 @@ class ResourceData extends foundry.abstract.TypeDataModel {
     static defineSchema() {
         const fields = foundry.data.fields;
         return {
-          quantity: new fields.NumberField({ integer: true, required: false, nullable: true, initial: 0})
+          quantity: new fields.NumberField({ integer: true, required: false, nullable: true, initial: 0}),
+          description: new fields.HTMLField({required: false, blank: true})
         };
       }
       prepareDerivedData() {
--- sheets/resource-sheet.js
+++ sheets/resource-sheet.js
@@ -1,29 +1,43 @@
 class ResourceSheet extends ItemSheet {
 	static get defaultOptions() {
 		return mergeObject(super.defaultOptions, {
-		  classes: ["sheet", "item", "simple-settlements-resource"],
-		  width: 520,
-		  height: 480,
-		  tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
+			classes: ["sheet", "item", "simple-settlements-resource"],
+			width: 520,
+			height: 480,
+			tabs: [
+				{
+					navSelector: ".sheet-tabs",
+					contentSelector: ".sheet-body",
+					initial: "description",
+				},
+			],
 		});
-	  }
+	}
 	get template() {
 		const path = "modules/simple-settlements/templates";
 		return `${path}/resource-sheet.html`;
 	}
-	getData() {
-		const data = super.getData();
-        console.log(data);
-		return data;
+	async getData(options) {
+		const context = super.getData(options);
+		(context.description = await TextEditor.enrichHTML(
+			this.object.system.description,
+			{
+				async: true,
+				secrets: this.object.isOwner,
+				relativeTo: this.object,
+			}
+		)),
+		console.log(context);
+		return context;
 	}
 	activateListeners(html) {
 		super.activateListeners(html);
-	  
+
 		// Everything below here is only needed if the sheet is editable
 		if (!this.options.editable) return;
-	  
+
 		// Roll handlers, click handlers, etc. would go here.
-	  }
+	}
 }
 
 export default ResourceSheet;
